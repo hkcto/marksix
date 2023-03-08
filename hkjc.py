697da193607f1ad34566e9565ab99d7965e77bcb
HKCTO <hksohoko@hotmail.com>
2023-03-08 20:57:18 +0800

    headless

--- hkjc.py
+++ hkjc.py
@@ -4,6 +4,7 @@ from marksix import markSix
 from gmailpy import Gmail
 
 
+
 def run(playwright: Playwright, marksix: list, order=False, headless=True) -> None:
     browser = playwright.chromium.launch(headless=headless)
     context = browser.new_context()
@@ -71,17 +72,17 @@ def run(playwright: Playwright, marksix: list, order=False, headless=True) -> No
             f.write(json.dumps(order_draw))
     else:
         print('Test Model')
+        page.pause()
     
-    page.pause()
     # page.pause()
     # ---------------------
     context.close()
     browser.close()
 
-def order(order=False):
+def order(order=False, headless=True):
     # 這個函數是為了給 run.py 調用
     with sync_playwright() as playwright:
-        run(playwright, marksix=markSix(), order=order)
+        run(playwright, marksix=markSix(), order=order, headless=headless)
 
 if __name__=='__main__':
     
--- run.py
+++ run.py
@@ -3,11 +3,14 @@ from record import Record
 import hkjc
 import argparse
 
-
 # -------------- 命令行傳入參數區 ---------------------------
 parser = argparse.ArgumentParser(description='order hkjc marksix')
-parser.add_argument('--order', action='store_true', help='order hkjc marksix')
+parser.add_argument('--order', action='store_true', default=False, help='order hkjc marksix')
+parser.add_argument('--day', type=int, default=1, help="買入時是否計算天數")
+parser.add_argument('--headless', action='store_true', default=False, help="顯示圖形")
 args = parser.parse_args()
+
+
 
 # -------------------- 下期攪珠日期 -------------------
 record = Record()
@@ -20,6 +23,8 @@ today = datetime.date.today()
 
 # ---------------------------- 六合彩落注 ------------------------
 if (today - next_draw_day).days == 0:
-    hkjc.order(order=args.order)
+    hkjc.order(order=args.order, headless=args.headless)
 else:
     print('離搞珠日還有:', today - next_draw_day)
+    if args.day == 0:
+        hkjc.order(order=args.order, headless=args.headless)
